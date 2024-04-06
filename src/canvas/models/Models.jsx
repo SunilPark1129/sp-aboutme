c61131edc006f0a21cac6aa81655dcb84eae5a4e
Sunil Park <sunilpark1129@gmail.com>
2024-04-06 15:02:17 -0400

    ADDED - art canvas model

--- src/canvas/models/Models.jsx
+++ src/canvas/models/Models.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Desktop from "./Desktop";
+import Art from "./Art";
 
 function Models() {
   return (
     <>
       <Desktop />
+      <Art />
     </>
   );
 }
